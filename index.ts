a9a84a84b3c331baf4baaded22b432ee97bbaf79
FormalSnake <kyaniserni@gmail.com>
2026-02-10 17:23:45 +0000

    fix(getStatus): parse and return JSON status instead of Response

--- index.ts
+++ index.ts
@@ -19,8 +19,9 @@ const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 async function getStatus(server: string) {
   const response = await fetch(`https://api.mcsrvstat.us/3/${server}`)
-  console.log(response.json())
-  return response
+  const status: ResponseType = await response.json()
+  console.log(status.motd.clean)
+  return status
 }
 
 // listen for the client to be ready
