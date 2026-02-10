67c2a71fe6a930f3b4f2a66f82e46b4480b1529f
FormalSnake <kyaniserni@gmail.com>
2026-02-10 18:06:45 +0000

    feat(mc-server): return players online instead of motd

--- utils/mc-server.ts
+++ utils/mc-server.ts
@@ -2,6 +2,7 @@ export async function getStatus(server: string) {
   const response = await fetch(`https://api.mcsrvstat.us/3/${server}`)
   const status: any = await response.json()
   // console.log(status.motd.clean)
-  return status.motd.clean
+  const statusString = `Players online: ${status.players.online}`
+  return statusString
 }
 
