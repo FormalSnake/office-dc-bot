2adcb761fbc4de356aeed1b5c10a9627f36c1004
FormalSnake <kyaniserni@gmail.com>
2026-02-11 09:07:16 +0000

    fix(mc-server): handle JSON errors and return fallback

--- utils/mc-server.ts
+++ utils/mc-server.ts
@@ -1,12 +1,16 @@
 export async function getStatus(server: string) {
   const response = await fetch(`https://api.mcsrvstat.us/3/${server}`)
-  const status: any = await response.json()
-  // console.log(status.motd.clean)
   var statusString: string
-  if (status.online) {
-    statusString = `Players online: ${status.players.online}`
-  } else {
-    statusString = "Server offline"
+  try {
+    const status: any = await response.json()
+    if (status.online) {
+      statusString = `Players online: ${status.players.online}`
+    } else {
+      statusString = "Server offline"
+    }
+  } catch (e) {
+    console.log(e)
+    statusString = "Could not fetch data"
   }
   return statusString
 }
